75215a0631ad91c74b7053048eab35b69fb9db0d
rodrigopaiv <rodrigopaiv@hotmail.com>
2024-11-08 13:43:51 -0300

    feat: etapa de inclusao no banco de dados concluída

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from contrato import Vendas
 from datetime import datetime, time
 from pydantic import ValidationError
+from database import salvar_no_postgres
 
 def main():
 
@@ -27,6 +28,7 @@ def main():
             )
 
             st.write(venda)
+            salvar_no_postgres(venda)
         except ValidationError as e:
             st.error(f"Deu Erro {e}")
 
